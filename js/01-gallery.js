15502b5106c7823c520a75ccb4531cf2691a43d9
svitlanakokorska <s.kokorskaya@gmail.com>
2023-03-07 19:01:43 +0200

    завдання перше

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,54 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
-
+const gallery = document.querySelector(".gallery");
 console.log(galleryItems);
+const items = [];
+
+//Перебирання масиву об'єктів методом forEach
+galleryItems.forEach((element) => {
+    
+  //Створення тегів галереї і класів до тегів
+  const galleryItem = document.createElement("div");
+  galleryItem.classList.add("gallery__item");
+
+  const galleryLink = document.createElement("a");
+  galleryLink.classList.add("gallery__link");
+
+  galleryLink.href = element.original;
+  const galleryImg = document.createElement("img");
+  galleryImg.classList.add("gallery__image");
+  galleryImg.src = element.preview;
+
+  galleryImg.setAttribute("data-source", element.original);
+  galleryImg.alt = element.description;
+
+  galleryItem.append(galleryLink);
+  galleryLink.append(galleryImg);
+  items.push(galleryItem);
+});
+gallery.append(...items);
+document.addEventListener("click", (e) => {
+  //По кліку користувач не буде перенаправлений на іншу сторінку
+  e.preventDefault();
+  if (e.target.nodeName !== "IMG") {
+    return;
+  }
+  const imgSelected = e.target.getAttribute("data-source");
+  const template = basicLightbox.create(
+    `<img src ="${imgSelected}" width="700" height="500">`,
+    {
+      onShow: () => {
+        document.addEventListener("keydown", closeModal);
+      },
+      onClose: () => {
+        document.removeEventListener("keydown", closeModal);
+      },
+    }
+  );
+  template.show();
+  function closeModal(e) {
+    if (e.key === "Escape") {
+      template.close();
+    }
+  }
+});
